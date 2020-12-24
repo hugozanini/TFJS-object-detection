153c6a0f9500a7a58d9facb545469574c70f55a7
hugozanini <hugozaninigomes@gmail.com>
2020-12-24 14:46:33 -0300

    Adding kangaroo detector

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ const threshold = 0.75;
 async function load_model() {
     // It's possible to load the model locally or from a repo
     //const model = await loadGraphModel("http://127.0.0.1:8080/model.json");
-    const model = await loadGraphModel("https://raw.githubusercontent.com/hugozanini/TFJS-object-detection/master/models/7500-96/model.json");
+    const model = await loadGraphModel("https://raw.githubusercontent.com/hugozanini/TFJS-object-detection/master/models/kangaroo-detector/model.json");
     return model;
   }
 
