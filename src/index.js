414a57c78341c536454010c853ef733dd3a32ff0
Hugo Zanini <hugozaninigomes@gmail.com>
2022-01-11 13:39:07 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ async function load_model() {
     // It's possible to load the model locally or from a repo
     // You can choose whatever IP and PORT you want in the "http://127.0.0.1:8080/model.json" just set it before in your https server
     //const model = await loadGraphModel("http://127.0.0.1:8080/model.json");
-    const model = await loadGraphModel("https://raw.githubusercontent.com/hugozanini/TFJS-object-detection/master/models/kangaroo-detector/model.json");
+    const model = await loadGraphModel("https://raw.githubusercontent.com/hugozanini/TFJS-object-detection/sku/models/model.json");
     return model;
   }
 
